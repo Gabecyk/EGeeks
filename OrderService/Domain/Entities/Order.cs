c3b17f96d3bef6a0130c0ad7a3dba119abc6db70
Gabriel Balduino <gabrielbalduino29@gmail.com>
2026-05-26 10:30:22 -0300

    feat: Adding a new column to the Order table

--- OrderService/Domain/Entities/Order.cs
+++ OrderService/Domain/Entities/Order.cs
@@ -11,7 +11,7 @@ public class Order
     public DateTime UpdateAt { get; set;}
 
     // Construtor sem parâmetros para EF Core
-    public Order() { }
+    private Order() { }
 
     public Order(Guid customerId, List<OrderItem> items)
     {
