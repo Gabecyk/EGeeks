b20591f46f5747f25bd2687f94ce581cdb093203
Gabriel Balduino <gabrielbalduino29@gmail.com>
2026-08-05 12:16:49 -0300

    hotfix: fixing auth service roles problem

--- Services/AuthService/Application/DTOs/RegisterRequester.cs
+++ Services/AuthService/Application/DTOs/RegisterRequester.cs
@@ -4,5 +4,5 @@ public record RegisterRequest(
     [Required] [StringLength(100, MinimumLength = 3)] string Name,
     [Required] [EmailAddress] string Email,
     [Required] [MinLength(8)] string Password,
-    [Required] string Role
+    [Required] [RegularExpression("^(Customer|Seller)$", ErrorMessage = "Role must be either 'Customer' or 'Seller'.")] string Role
 );
